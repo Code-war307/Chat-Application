4444a74fc63d16c9ddf8c77c437d7db774568fa2
code-war307 <uttampradhan115@gmail.com>
2025-07-05 13:01:01 +0530

    Update axios.js

--- get-chat/lib/axios.js
+++ get-chat/lib/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 const url = process.env.NEXT_PUBLIC_BACKEND_URL
 export const axiosInstance = axios.create({
     baseURL: `${url}/api`,
-    withCredentials: true
+    //withCredentials: true
 })
