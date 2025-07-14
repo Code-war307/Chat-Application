03ba18126bf9a4550ce426a157697c8807c7d559
code-war307 <uttampradhan115@gmail.com>
2025-07-14 05:48:49 +0530

    Update redis.js

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,7 +1,7 @@
 import {createClient} from 'redis'
 
-const url = process.env.REDIS_URL
-const redis = createClient(url)
+const redisUrl = process.env.REDIS_URL
+const redis = createClient({url: redisUrl})
 redis.on('error', (err) => console.error('Redis error occur : ',err))
 redis.on('connect', () => console.log('Redis connected succesfully'))
 await redis.connect()
